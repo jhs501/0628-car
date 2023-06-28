a8a1893850e804788ce19db3274e468587bb178c
jhs501 <137017639+jhs501@users.noreply.github.com>
2023-06-28 14:56:16 +0900

    Update index.js
    c2k

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
       <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
-        pmnd.rs
+        c2k형석
         <br />
-        dev collective
+        c2k
       </a>
       <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>ok —</div>
       <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>22/12/2022</div>
